146ead42795caa9fd21ad474db117c4dc8529fbc
Julio Cesar Aguero <julio.aguero.gomez@gmail.com>
2022-03-09 09:24:39 -0600

    eliminacion de vistas generales

--- src/router/index.js
+++ src/router/index.js
@@ -14,56 +14,6 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/VistaGeneral.vue')
   },
   {
-    path: '/nosotros',
-    name: 'Nosotros',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Nosotros.vue')
-  },
-  {
-    path: '/marcas',
-    name: 'Marcas',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Marcas.vue')
-  },
-  {
-    path: '/beneficios',
-    name: 'Beneficios',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Beneficios.vue')
-  }
-  ,
-  {
-    path: '/proyectos',
-    name: 'Proyectos',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Proyectos_especiales.vue')
-  } ,
-  {
-    path: '/slider',
-    name: 'Slider',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Slider.vue')
-  } ,
-  {
-    path: '/clientes',
-    name: 'Clientes',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Clientes.vue')
-  }
-  ,
-  {
     path: '/cotizador',
     name: 'Cotizador',
     // route level code-splitting
